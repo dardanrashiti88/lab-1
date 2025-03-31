1c35ae5d3a54975d91c61c3c1dd0c893e80f340b
dardanrashiti88 <rashitidardan3@gmail.com>
2025-03-31 20:45:21 +0200

    Add files via upload

---
+++ db/02-seed-table.sql
@@ -0,0 +1,16 @@
+INSERT INTO players (name, position, matches, goals, assists) VALUES
+('Mohamed Salah', 'Forward', 30, 19, 8),
+('Virgil van Dijk', 'Defender', 28, 3, 1),
+('Trent Alexander-Arnold', 'Defender', 27, 2, 10),
+('Alisson Becker', 'Goalkeeper', 29, 0, 0),
+('Diogo Jota', 'Forward', 25, 10, 4),
+('Darwin Núñez', 'Forward', 26, 12, 6),
+('Dominik Szoboszlai', 'Midfielder', 23, 5, 7),
+('Andrew Robertson', 'Defender', 22, 1, 5),
+('Curtis Jones', 'Midfielder', 24, 4, 4),
+('Luis Díaz', 'Forward', 27, 8, 3);
+
+INSERT INTO champions_league (season, winner, mvp, top_scorer, goals) VALUES
+(2018, 'Liverpool FC', 'Virgil van Dijk', 'Mohamed Salah', 5),
+(2019, 'Bayern Munich', 'Robert Lewandowski', 'Robert Lewandowski', 15),
+(2020, 'Chelsea FC', 'N'Golo Kanté', 'Erling Haaland', 10);
